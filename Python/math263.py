2f2c73598d6b9055380acfec75c6417b9a3c1b5e
Ethan Smith <ethancsmith@gmail.com>
2024-05-17 21:50:35 -0400

    starts IPYNB for euler method

--- Python/math263.py
+++ Python/math263.py
@@ -3,14 +3,14 @@ import numpy as np
 def feuler(f, a, b, y0, n):
 	'''
 	numerically solves IVP
-		y' = f(x,y), y(0)=y0
+		y' = f(x,y), y(a)=y0
 	over the interval [a, b] via n steps of the (foward) euler method 
 	'''
 	h = (b-a)/n;
 	x = np.linspace(a, b, num=n+1);
 	y = np.zeros(x.size);
 	y[0] = y0;
-	for k in range(n):
-		y[k+1] = y[k] + h * f(x[k], y[k]);
+	for i in range(n):
+		y[i+1] = y[i] + h * f(x[i], y[i]);
 
 	return (x, y)
