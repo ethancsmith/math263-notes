bb49341adb52d47d233a6810c8c94b9a837abae6
ethancsmith <ethancsmith@gmail.com>
2025-02-24 20:34:46 -0500

    applies black and isort formatters to test scripts

--- make_test_data.py
+++ make_test_data.py
@@ -1,59 +1,60 @@
-import math263
-import yaml
 import numpy as np
+import yaml
+
+import math263
 
 # scalar test input parameters
-f = lambda x, y: x**2 - y;
-a, b = 0, 2;
-y0 = 3;
-n = 10;
+f = lambda x, y: x**2 - y
+a, b = 0, 2
+y0 = 3
+n = 10
 
-test_dict = dict();
+test_dict = dict()
 
 # make data
-(xi, yi) = math263.euler(f, a, b, y0, n);
-test_dict["euler scalar test"] = {"xi": xi.tolist(), "yi": yi.tolist()};
+(xi, yi) = math263.euler(f, a, b, y0, n)
+test_dict["euler scalar test"] = {"xi": xi.tolist(), "yi": yi.tolist()}
 
-(xi, yi) = math263.mem(f, a, b, y0, n);
-test_dict["mem scalar test"] = {"xi": xi.tolist(), "yi": yi.tolist()};
+(xi, yi) = math263.mem(f, a, b, y0, n)
+test_dict["mem scalar test"] = {"xi": xi.tolist(), "yi": yi.tolist()}
 
-(xi, yi) = math263.bem(f, a, b, y0, n);
-test_dict["bem scalar test"] = {"xi": xi.tolist(), "yi": yi.tolist()};
+(xi, yi) = math263.bem(f, a, b, y0, n)
+test_dict["bem scalar test"] = {"xi": xi.tolist(), "yi": yi.tolist()}
 
-(xi, yi) = math263.rk4(f, a, b, y0, n);
-test_dict["rk4 scalar test"] = {"xi": xi.tolist(), "yi": yi.tolist()};
+(xi, yi) = math263.rk4(f, a, b, y0, n)
+test_dict["rk4 scalar test"] = {"xi": xi.tolist(), "yi": yi.tolist()}
 
-(xi, yi) = math263.ab2(f, a, b, y0, n);
-test_dict["ab2 scalar test"] = {"xi": xi.tolist(), "yi": yi.tolist()};
+(xi, yi) = math263.ab2(f, a, b, y0, n)
+test_dict["ab2 scalar test"] = {"xi": xi.tolist(), "yi": yi.tolist()}
 
-(xi, yi) = math263.abm2(f, a, b, y0, n);
-test_dict["abm2 scalar test"] = {"xi": xi.tolist(), "yi": yi.tolist()};
+(xi, yi) = math263.abm2(f, a, b, y0, n)
+test_dict["abm2 scalar test"] = {"xi": xi.tolist(), "yi": yi.tolist()}
 
 # vector test input parameters
-f = lambda t, r: np.array([r[0] + r[2], r[0] + r[1], -2*r[0] - r[2]]);
-a, b = 0, 2*np.pi;
-r0=np.array([1, -1/2, -1]);
-n = 10;
+f = lambda t, r: np.array([r[0] + r[2], r[0] + r[1], -2 * r[0] - r[2]])
+a, b = 0, 2 * np.pi
+r0 = np.array([1, -1 / 2, -1])
+n = 10
 
 # make data
-(xi, yi) = math263.euler(f, a, b, r0, n);
-test_dict["euler vector test"] = {"xi": xi.tolist(), "yi": yi.tolist()};
+(xi, yi) = math263.euler(f, a, b, r0, n)
+test_dict["euler vector test"] = {"xi": xi.tolist(), "yi": yi.tolist()}
 
-(xi, yi) = math263.mem(f, a, b, r0, n);
-test_dict["mem vector test"] = {"xi": xi.tolist(), "yi": yi.tolist()};
+(xi, yi) = math263.mem(f, a, b, r0, n)
+test_dict["mem vector test"] = {"xi": xi.tolist(), "yi": yi.tolist()}
 
-(xi, yi) = math263.bem(f, a, b, r0, n);
-test_dict["bem vector test"] = {"xi": xi.tolist(), "yi": yi.tolist()};
+(xi, yi) = math263.bem(f, a, b, r0, n)
+test_dict["bem vector test"] = {"xi": xi.tolist(), "yi": yi.tolist()}
 
-(xi, yi) = math263.rk4(f, a, b, r0, n);
-test_dict["rk4 vector test"] = {"xi": xi.tolist(), "yi": yi.tolist()};
+(xi, yi) = math263.rk4(f, a, b, r0, n)
+test_dict["rk4 vector test"] = {"xi": xi.tolist(), "yi": yi.tolist()}
 
-(xi, yi) = math263.ab2(f, a, b, r0, n);
-test_dict["ab2 vector test"] = {"xi": xi.tolist(), "yi": yi.tolist()};
+(xi, yi) = math263.ab2(f, a, b, r0, n)
+test_dict["ab2 vector test"] = {"xi": xi.tolist(), "yi": yi.tolist()}
 
-(xi, yi) = math263.abm2(f, a, b, r0, n);
-test_dict["abm2 vector test"] = {"xi": xi.tolist(), "yi": yi.tolist()};
+(xi, yi) = math263.abm2(f, a, b, r0, n)
+test_dict["abm2 vector test"] = {"xi": xi.tolist(), "yi": yi.tolist()}
 
 # write date to YAML file
 with open("test_data.yaml", "w") as fp:
-    yaml.safe_dump(test_dict, fp);
+    yaml.safe_dump(test_dict, fp)
--- test_math263.py
+++ test_math263.py
@@ -1,114 +1,191 @@
-import math263
+import unittest
+
 import numpy
 import yaml
-import unittest
+
+import math263
 
 # read expected outputs from file
 with open("test_data.yaml", "r") as fp:
-    test_dict = yaml.safe_load(fp);
+    test_dict = yaml.safe_load(fp)
 
 # test parameters for scalar IVP test
-f = lambda x, y: x**2 - y;
+f = lambda x, y: x**2 - y
+
 
 class ScalarTestODESolvers(unittest.TestCase):
 
-    a, b = 0, 2;
-    y0 = 3;
-    n = 10;
+    a, b = 0, 2
+    y0 = 3
+    n = 10
 
     def test_euler(self):
-        (x_obs, y_obs) = math263.euler(f, self.a, self.b, self.y0, self.n);
-        x_exp = test_dict["euler scalar test"]["xi"];
-        y_exp = test_dict["euler scalar test"]["yi"];
-        self.assertTrue(numpy.allclose(x_obs, x_exp), "math263.euler produces incorrect x-values on test");
-        self.assertTrue(numpy.allclose(y_obs, y_exp), "math263.euler produces incorrect y-values on test");
+        (x_obs, y_obs) = math263.euler(f, self.a, self.b, self.y0, self.n)
+        x_exp = test_dict["euler scalar test"]["xi"]
+        y_exp = test_dict["euler scalar test"]["yi"]
+        self.assertTrue(
+            numpy.allclose(x_obs, x_exp),
+            "math263.euler produces incorrect x-values on test",
+        )
+        self.assertTrue(
+            numpy.allclose(y_obs, y_exp),
+            "math263.euler produces incorrect y-values on test",
+        )
 
     def test_mem(self):
-        (x_obs, y_obs) = math263.mem(f, self.a, self.b, self.y0, self.n);
-        x_exp = test_dict["mem scalar test"]["xi"];
-        y_exp = test_dict["mem scalar test"]["yi"];
-        self.assertTrue(numpy.allclose(x_obs, x_exp), "math263.mem produces incorrect x-values on test");
-        self.assertTrue(numpy.allclose(y_obs, y_exp), "math263.mem produces incorrect y-values on test");
+        (x_obs, y_obs) = math263.mem(f, self.a, self.b, self.y0, self.n)
+        x_exp = test_dict["mem scalar test"]["xi"]
+        y_exp = test_dict["mem scalar test"]["yi"]
+        self.assertTrue(
+            numpy.allclose(x_obs, x_exp),
+            "math263.mem produces incorrect x-values on test",
+        )
+        self.assertTrue(
+            numpy.allclose(y_obs, y_exp),
+            "math263.mem produces incorrect y-values on test",
+        )
 
     def test_bem(self):
-        (x_obs, y_obs) = math263.bem(f, self.a, self.b, self.y0, self.n);
-        x_exp = test_dict["bem scalar test"]["xi"];
-        y_exp = test_dict["bem scalar test"]["yi"];
-        self.assertTrue(numpy.allclose(x_obs, x_exp), "math263.bem produces incorrect x-values on test");
-        self.assertTrue(numpy.allclose(y_obs, y_exp), "math263.bem produces incorrect y-values on test");
+        (x_obs, y_obs) = math263.bem(f, self.a, self.b, self.y0, self.n)
+        x_exp = test_dict["bem scalar test"]["xi"]
+        y_exp = test_dict["bem scalar test"]["yi"]
+        self.assertTrue(
+            numpy.allclose(x_obs, x_exp),
+            "math263.bem produces incorrect x-values on test",
+        )
+        self.assertTrue(
+            numpy.allclose(y_obs, y_exp),
+            "math263.bem produces incorrect y-values on test",
+        )
 
     def test_rk4(self):
-        (x_obs, y_obs) = math263.rk4(f, self.a, self.b, self.y0, self.n);
-        x_exp = test_dict["rk4 scalar test"]["xi"];
-        y_exp = test_dict["rk4 scalar test"]["yi"];
-        self.assertTrue(numpy.allclose(x_obs, x_exp), "math263.rk4 produces incorrect x-values on test");
-        self.assertTrue(numpy.allclose(y_obs, y_exp), "math263.rk4 produces incorrect y-values on test");
+        (x_obs, y_obs) = math263.rk4(f, self.a, self.b, self.y0, self.n)
+        x_exp = test_dict["rk4 scalar test"]["xi"]
+        y_exp = test_dict["rk4 scalar test"]["yi"]
+        self.assertTrue(
+            numpy.allclose(x_obs, x_exp),
+            "math263.rk4 produces incorrect x-values on test",
+        )
+        self.assertTrue(
+            numpy.allclose(y_obs, y_exp),
+            "math263.rk4 produces incorrect y-values on test",
+        )
 
     def test_ab2(self):
-        (x_obs, y_obs) = math263.ab2(f, self.a, self.b, self.y0, self.n);
-        x_exp = test_dict["ab2 scalar test"]["xi"];
-        y_exp = test_dict["ab2 scalar test"]["yi"];
-        self.assertTrue(numpy.allclose(x_obs, x_exp), "math263.ab2 produces incorrect x-values on test");
-        self.assertTrue(numpy.allclose(y_obs, y_exp), "math263.ab2 produces incorrect y-values on test");
+        (x_obs, y_obs) = math263.ab2(f, self.a, self.b, self.y0, self.n)
+        x_exp = test_dict["ab2 scalar test"]["xi"]
+        y_exp = test_dict["ab2 scalar test"]["yi"]
+        self.assertTrue(
+            numpy.allclose(x_obs, x_exp),
+            "math263.ab2 produces incorrect x-values on test",
+        )
+        self.assertTrue(
+            numpy.allclose(y_obs, y_exp),
+            "math263.ab2 produces incorrect y-values on test",
+        )
 
     def test_abm2(self):
-        (x_obs, y_obs) = math263.abm2(f, self.a, self.b, self.y0, self.n);
-        x_exp = test_dict["abm2 scalar test"]["xi"];
-        y_exp = test_dict["abm2 scalar test"]["yi"];
-        self.assertTrue(numpy.allclose(x_obs, x_exp), "math263.abm2 produces incorrect x-values on test");
-        self.assertTrue(numpy.allclose(y_obs, y_exp), "math263.abm2 produces incorrect y-values on test");
+        (x_obs, y_obs) = math263.abm2(f, self.a, self.b, self.y0, self.n)
+        x_exp = test_dict["abm2 scalar test"]["xi"]
+        y_exp = test_dict["abm2 scalar test"]["yi"]
+        self.assertTrue(
+            numpy.allclose(x_obs, x_exp),
+            "math263.abm2 produces incorrect x-values on test",
+        )
+        self.assertTrue(
+            numpy.allclose(y_obs, y_exp),
+            "math263.abm2 produces incorrect y-values on test",
+        )
+
 
 # test parameters for vector IVP test
-vec_f = lambda t, r: numpy.array([r[0] + r[2], r[0] + r[1], -2*r[0] - r[2]]);
+vec_f = lambda t, r: numpy.array([r[0] + r[2], r[0] + r[1], -2 * r[0] - r[2]])
+
 
 class VectorTestODESolvers(unittest.TestCase):
 
-    a, b = 0, 2*numpy.pi;
-    r0=numpy.array([1, -1/2, -1]);
-    n = 10;
+    a, b = 0, 2 * numpy.pi
+    r0 = numpy.array([1, -1 / 2, -1])
+    n = 10
 
     def test_euler(self):
-        (x_obs, y_obs) = math263.euler(vec_f, self.a, self.b, self.r0, self.n);
-        x_exp = test_dict["euler vector test"]["xi"];
-        y_exp = test_dict["euler vector test"]["yi"];
-        self.assertTrue(numpy.allclose(x_obs, x_exp), "math263.euler produces incorrect x-values on test");
-        self.assertTrue(numpy.allclose(y_obs, y_exp), "math263.euler produces incorrect y-values on test");
+        (x_obs, y_obs) = math263.euler(vec_f, self.a, self.b, self.r0, self.n)
+        x_exp = test_dict["euler vector test"]["xi"]
+        y_exp = test_dict["euler vector test"]["yi"]
+        self.assertTrue(
+            numpy.allclose(x_obs, x_exp),
+            "math263.euler produces incorrect x-values on test",
+        )
+        self.assertTrue(
+            numpy.allclose(y_obs, y_exp),
+            "math263.euler produces incorrect y-values on test",
+        )
 
     def test_mem(self):
-        (x_obs, y_obs) = math263.mem(vec_f, self.a, self.b, self.r0, self.n);
-        x_exp = test_dict["mem vector test"]["xi"];
-        y_exp = test_dict["mem vector test"]["yi"];
-        self.assertTrue(numpy.allclose(x_obs, x_exp), "math263.mem produces incorrect x-values on test");
-        self.assertTrue(numpy.allclose(y_obs, y_exp), "math263.mem produces incorrect y-values on test");
+        (x_obs, y_obs) = math263.mem(vec_f, self.a, self.b, self.r0, self.n)
+        x_exp = test_dict["mem vector test"]["xi"]
+        y_exp = test_dict["mem vector test"]["yi"]
+        self.assertTrue(
+            numpy.allclose(x_obs, x_exp),
+            "math263.mem produces incorrect x-values on test",
+        )
+        self.assertTrue(
+            numpy.allclose(y_obs, y_exp),
+            "math263.mem produces incorrect y-values on test",
+        )
 
     def test_bem(self):
-        (x_obs, y_obs) = math263.bem(vec_f, self.a, self.b, self.r0, self.n);
-        x_exp = test_dict["bem vector test"]["xi"];
-        y_exp = test_dict["bem vector test"]["yi"];
-        self.assertTrue(numpy.allclose(x_obs, x_exp), "math263.bem produces incorrect x-values on test");
-        self.assertTrue(numpy.allclose(y_obs, y_exp), "math263.bem produces incorrect y-values on test");
+        (x_obs, y_obs) = math263.bem(vec_f, self.a, self.b, self.r0, self.n)
+        x_exp = test_dict["bem vector test"]["xi"]
+        y_exp = test_dict["bem vector test"]["yi"]
+        self.assertTrue(
+            numpy.allclose(x_obs, x_exp),
+            "math263.bem produces incorrect x-values on test",
+        )
+        self.assertTrue(
+            numpy.allclose(y_obs, y_exp),
+            "math263.bem produces incorrect y-values on test",
+        )
 
     def test_rk4(self):
-        (x_obs, y_obs) = math263.rk4(vec_f, self.a, self.b, self.r0, self.n);
-        x_exp = test_dict["rk4 vector test"]["xi"];
-        y_exp = test_dict["rk4 vector test"]["yi"];
-        self.assertTrue(numpy.allclose(x_obs, x_exp), "math263.rk4 produces incorrect x-values on test");
-        self.assertTrue(numpy.allclose(y_obs, y_exp), "math263.rk4 produces incorrect y-values on test");
+        (x_obs, y_obs) = math263.rk4(vec_f, self.a, self.b, self.r0, self.n)
+        x_exp = test_dict["rk4 vector test"]["xi"]
+        y_exp = test_dict["rk4 vector test"]["yi"]
+        self.assertTrue(
+            numpy.allclose(x_obs, x_exp),
+            "math263.rk4 produces incorrect x-values on test",
+        )
+        self.assertTrue(
+            numpy.allclose(y_obs, y_exp),
+            "math263.rk4 produces incorrect y-values on test",
+        )
 
     def test_ab2(self):
-        (x_obs, y_obs) = math263.ab2(vec_f, self.a, self.b, self.r0, self.n);
-        x_exp = test_dict["ab2 vector test"]["xi"];
-        y_exp = test_dict["ab2 vector test"]["yi"];
-        self.assertTrue(numpy.allclose(x_obs, x_exp), "math263.ab2 produces incorrect x-values on test");
-        self.assertTrue(numpy.allclose(y_obs, y_exp), "math263.ab2 produces incorrect y-values on test");
+        (x_obs, y_obs) = math263.ab2(vec_f, self.a, self.b, self.r0, self.n)
+        x_exp = test_dict["ab2 vector test"]["xi"]
+        y_exp = test_dict["ab2 vector test"]["yi"]
+        self.assertTrue(
+            numpy.allclose(x_obs, x_exp),
+            "math263.ab2 produces incorrect x-values on test",
+        )
+        self.assertTrue(
+            numpy.allclose(y_obs, y_exp),
+            "math263.ab2 produces incorrect y-values on test",
+        )
 
     def test_abm2(self):
-        (x_obs, y_obs) = math263.abm2(vec_f, self.a, self.b, self.r0, self.n);
-        x_exp = test_dict["abm2 vector test"]["xi"];
-        y_exp = test_dict["abm2 vector test"]["yi"];
-        self.assertTrue(numpy.allclose(x_obs, x_exp), "math263.abm2 produces incorrect x-values on test");
-        self.assertTrue(numpy.allclose(y_obs, y_exp), "math263.abm2 produces incorrect y-values on test");
+        (x_obs, y_obs) = math263.abm2(vec_f, self.a, self.b, self.r0, self.n)
+        x_exp = test_dict["abm2 vector test"]["xi"]
+        y_exp = test_dict["abm2 vector test"]["yi"]
+        self.assertTrue(
+            numpy.allclose(x_obs, x_exp),
+            "math263.abm2 produces incorrect x-values on test",
+        )
+        self.assertTrue(
+            numpy.allclose(y_obs, y_exp),
+            "math263.abm2 produces incorrect y-values on test",
+        )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main(verbosity=2)
-
